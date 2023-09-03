23df107c81a6f0eb6675ece79fff926910cbe2db
R0P1 <139617581+R0P1@users.noreply.github.com>
2023-09-04 01:25:37 +0700

    Update bafi.bat

--- bafi.bat
+++ bafi.bat
@@ -5,6 +5,7 @@ echo.
 echo ----------------------------- BAFI -----------------------------                                                                                                 
 echo Collect information about WI-FI profiles stored on the computer.
 echo ----------------------------------------------------------------
+echo                                 https://github.com/r0p1/bafi.git
 netsh wlan show profile
 set /p user_profile=[BAFI] Select a user profile (or type 'E' to exit): 
 if "%user_profile%"=="E" (
