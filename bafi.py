c17493dbe7f40f298e0f458925d9e2405ec2affd
R0P1 <139617581+R0P1@users.noreply.github.com>
2023-09-05 19:05:22 +0700

    Update bafi.py

--- bafi.py
+++ bafi.py
@@ -2,11 +2,13 @@ import os
 
 while True:
     os.system("cls")
-    perintah = "netsh wlan show profile"
-    os.system(perintah)
+    command = "netsh wlan show profile"
+    os.system(command)
     user_profile = input("Select a user profie (or type 'E' to exit): ")
     if user_profile == "E":
         exit(0)
     else:
+        
         os.system(f"{perintah} {user_profile} key=clear")
+        c = input("Press any key to continue...")
         continue
