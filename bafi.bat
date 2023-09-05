8f81c6b0faa1e927b29f6bef926291b85db5ec8b
R0P1 <139617581+R0P1@users.noreply.github.com>
2023-09-05 18:22:19 +0700

    Update bafi.bat

--- bafi.bat
+++ bafi.bat
@@ -14,7 +14,7 @@ echo                                 https://github.com/r0p1/bafi.git
 netsh wlan show profile
 set /p user_profile=[BAFI] Select a user profile (or type 'E' to exit): 
 if "%user_profile%"=="E" (
-    exit 0
+    exit /b 0
 )
 netsh wlan show profile %user_profile% key=clear
 pause
